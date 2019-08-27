2603c9d660d25c695175f6ddb81a57b4307634ba
eladeyal-intel <elad.eyal@intel.com>
2019-08-27 16:30:30 +0300

    Add review_score; allows editing review_score

--- app/models/review_metric.rb
+++ app/models/review_metric.rb
@@ -1,5 +1,6 @@
 class ReviewMetric < ApplicationRecord
   belongs_to :conference
+  has_many :review_score, dependent: :destroy
   
   validates :name, presence: true, uniqueness: { scope: :conference }
 end
