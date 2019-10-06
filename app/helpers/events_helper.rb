c97311c58f3b9db5f83c58c1069fe6d5dcda82ef
eladeyal <elad.eyal@intel.com>
2019-10-06 17:07:20 +0300

    numeric events filters; i18n event lists
    see frab#576

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -24,4 +24,35 @@ module EventsHelper
     duration_in_minutes = number_of_time_slots * @conference.timeslot_duration
     duration_to_time(duration_in_minutes)
   end
+  
+  def supported_filters
+    supported_exact_filters + supported_numeric_filters
+  end
+  
+  def supported_exact_filters
+    #      attribute/sql name       query parameter        huuman_name                                             scope_for_values
+    [ [    :track_name,             'track_name',          t('activerecord.attributes.event.track'),               nil                  ],
+      [    :event_type,             'event_type',          t('activerecord.attributes.event.event_type'),          'options' ] ,
+      [    :event_state,            'event_state',         t('activerecord.attributes.event.state'),               'conferences_module' ] ]
+  end                                                  
+                                                       
+  def supported_numeric_filters                        
+    #      attribute/sql name       query parameter        human_name                                              scope_for_values
+    [  [   :average_rating,         'average_rating',      t('activerecord.attributes.event.average_rating'),      nil                  ],
+       [   :event_ratings_count,    'event_ratings_count', t('activerecord.attributes.event.event_ratings_count'), nil                  ] ] +
+    @conference.review_metrics.map{ |rm| 
+       [   "#{rm.safe_name}.score", rm.safe_name,          rm.name,                                                nil                  ] }
+  end
+  
+  def show_filters_pane
+    supported_filters.each do |attrname, qname, human_name, scope|
+      return true if params[qname].present?
+    end
+    false
+  end
+  
+  def get_op_and_val(str)
+    /^(?<op>[≤≥=]?)(?<val>.*)$/ =~ str
+    return op, val
+  end
 end
