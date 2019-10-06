c85cadc564f0b94359d5ec9aa5ae32f7ef77c6bc
eladeyal <elad.eyal@intel.com>
2019-10-06 17:07:20 +0300

    numeric events filters; i18n event lists
    see frab#576

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -260,6 +260,15 @@ class EventsController < BaseConferenceController
     filter = filter.where(state: params[:event_state]) if params[:event_state].present?
     filter = filter.where(event_type: params[:event_type]) if params[:event_type].present?
     filter = filter.where(track: @conference.tracks.find_by(:name => params[:track_name])) if params[:track_name].present?
+    helpers.supported_numeric_filters.each do |attrname, qname, human_name, i18n_scope|
+      if params[qname].present?
+        op,val = helpers.get_op_and_val(params[qname])
+        op = { '≥' => '>=', 
+               '=' => '=', 
+               '≤' => '<=' }[op]
+        filter = filter.where("#{attrname} #{op} #{val}") if op
+      end
+    end
     @search = perform_search(filter, params, %i(title_cont description_cont abstract_cont track_name_cont event_type_is))
     if params.dig('q', 's')&.match('track_name')
       @search.result
