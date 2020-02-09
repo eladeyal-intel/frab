08d98a399ad1699d1e70d6c7641c428daaf3bfd6
Emir Isman <9727151+emirisman@users.noreply.github.com>
2020-02-09 23:02:38 +0300

    Add test for OpenID Connect

---
+++ test/features/openid_connect_test.rb
@@ -0,0 +1,41 @@
+require 'test_helper'
+
+class OpenIDConnectTest < FeatureTest
+  LOGIN='alice'
+  PASSWORD='secret'
+  EMAIL='alice@wonderland.net'
+
+  setup do
+    assert User.where(email: EMAIL).blank?
+  end
+
+  def connect_with_openid_connect
+    visit '/'
+   
+    click_on 'Log-in'
+   
+    click_on 'Sign in with free testing server at demo.c2id.com'
+    follow_redirect! 
+
+    fill_in 'username', with: LOGIN
+    fill_in 'password', with: PASSWORD
+    click_on 'Login'
+    follow_redirect!
+    
+    assert_content page, 'Successfully authenticated'
+    assert User.where(email: EMAIL).any?
+    
+    click_on 'Logout'
+  end
+
+# Currently testing only once - second test would not result
+# in a new login prompt 
+  test 'can sign up and sign in with openid connect' do
+    connect_with_openid_connect
+  end
+
+  teardown do
+    User.where(email: EMAIL).destroy_all
+  end
+end
+
